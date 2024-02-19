6f9603546f97d261a2969ff32f42f9df958a3afa
Joakim Bohlin <joakim.bohlin@chalmers.se>
2024-02-19 16:45:31 +0100

    Fix tree positioning issues; avoid teleportation

--- src/Patch.js
+++ src/Patch.js
@@ -32,9 +32,9 @@ class Patch {
         this.allCells = new Map();
         this.availableCells = new Map();
         this.allowedOverlap = 0.5; // Allow 50% overlap
+        this.cellSide = 0.3;
 
         this.positionedYears = new Set();
-
 
         const minGrassThickness = 1;
         const grassGeometry = new THREE.BoxGeometry(
@@ -57,76 +57,46 @@ class Patch {
     }
 
     initTreePositions(year) {
-        // Create a list of all trees and their crown radii
-        const trees = [];
-        let totalCrownArea = 0;
-
-        // Update cohorts with the largest trees first
-        const cohorts = [...this.cohorts.values()].filter(c=> !c.isGrass && c.timeSteps.has(year));
-        cohorts.sort((a,b) => b.timeSteps.get(year).CrownA - a.timeSteps.get(year).CrownA);
-
-        for (const cohort of cohorts) {
-            if (cohort.isGrass || !cohort.timeSteps.has(year)) {
-                continue;
-            }
-            const d = cohort.timeSteps.get(year)
-            const crownRadius = Math.sqrt(d.CrownA/Math.PI);
-            const nTreesInCohort = d.DensI * cohort.maxTreeCount;
-            for (let i=0; i<nTreesInCohort; i++) {
-                trees.push({
-                    data: d,
-                    crownRadius: crownRadius,
-                    instanceId: i
-                });
-                totalCrownArea += d.CrownA;
-            }
-        }
-
         this.availableCells.set(year, []);
         this.allCells.set(year, []);
 
-        const delta = 0.5 //(this.area / trees.length);
         // Divide the patch into list of available cells
-        for (let x=0; x<this.sideLength; x+=delta) {
-            for (let y=0; y<this.sideLength; y+=delta) {
+        for (let x=0; x<this.sideLength; x+=this.cellSide) {
+            for (let y=0; y<this.sideLength; y+=this.cellSide) {
                 const cell = new PatchCell(x, y);
                 this.allCells.get(year).push(cell);
                 this.availableCells.get(year).push(cell);
             }
         }
-        // Place trees one by one and remove all cells they occupy
-        for (const tree of trees) {
-            const cell = randItem(this.availableCells.get(year));
-            this.availableCells.set(year, this.availableCells.get(year).filter(
-                c=>cell.distanceTo(c) > tree.crownRadius * this.allowedOverlap
-            ));
-            for (const c of this.allCells) {
-                if (cell.distanceTo(c) <= tree.crownRadius * this.allowedOverlap) {
-                    cell.occupyingCohort = idFromData(tree.data);
-                    cell.occupyingInstance = tree.instanceId;
-                }
-            }
-            const cohortData = this.cohorts.get(idFromData(tree.data)).timeSteps.get(year)
-            cohortData.positions.set(tree.instanceId, cell);
-        }
-
-        this.positionedYears.add(year);
     }
 
     updateTreePositions(year) {
         // Only need to do this once per year
         if (this.positionedYears.has(year)) {
-            return
+            return;
         }
 
-        this.availableCells.set(year, []);
-        this.allCells.set(year, []);
+        // The year might be initialised with cells, but with no
+        // positions. So don't overvrite anything
+        if (!this.availableCells.has(year)) {
+            this.availableCells.set(year, []);
+        }
+        if (!this.allCells.has(year)) {
+            this.allCells.set(year, []);
+        }
 
         // Use positions from a previous year if we have them
         const prevYears = [...this.positionedYears.values()].filter(y => y < year);
         if (prevYears.length > 0) {
-            const lastYear = Math.max(prevYears);
-            this.allCells.set(year, this.availableCells.get(lastYear).map(c=>c.clone()));
+            // Get most recent year and copy cells
+            const lastYear = Math.max(...prevYears);
+            for (const c of this.allCells.get(lastYear)) {
+                // Don't use positions for cohorts that will be removed
+                if (c.occupyingCohort === undefined || c.occupyingCohort.timeSteps.has(year)) {
+                    this.allCells.get(year).push(c.clone());
+                }
+            }
+            //this.allCells.set(year, this.allCells.get(lastYear).map(c=>c.clone()));
             this.availableCells.set(
                 year, this.allCells.get(year).filter(c=>(
                     c.occupyingCohort === undefined &&
@@ -134,6 +104,7 @@ class Patch {
                 )
             ));
             for (const c of this.cohorts.values()) {
+                // Also copy positions (instead of starting from scratch)
                 if (c.timeSteps.has(year) && c.timeSteps.has(lastYear)) {
                     c.timeSteps.get(lastYear).positions.forEach((cell, instanceId)=> {
                         c.timeSteps.get(year).positions.set(instanceId, cell)
@@ -142,47 +113,51 @@ class Patch {
             }
         }
 
+        // Get cohort array that we can filter and sort
+        let cohorts = [...this.cohorts.values()];
+        // Ignore grass and cohorts not present this year
+        cohorts = cohorts.filter(c=> !c.isGrass && c.timeSteps.has(year));
         // Update cohorts with the largest trees first
-        const cohorts = [...this.cohorts.values()].filter(c=> !c.isGrass && c.timeSteps.has(year));
         cohorts.sort((a,b) => b.timeSteps.get(year).CrownA - a.timeSteps.get(year).CrownA);
 
         // Remove any extra trees in each cohort
         for (const cohort of cohorts) {
-            const d = cohort.timeSteps.get(year)
+            const d = cohort.timeSteps.get(year);
             const nTreesInCohort = d.DensI * cohort.maxTreeCount;
 
             // If we have more tree positions defined than we should
             if (d.positions.size > nTreesInCohort) {
-                console.log(`Removing ${d.positions.size - nTreesInCohort} trees from cohort ${d.IID}, patch ${d.PID}. Available cells: ${this.availableCells.get(year).length}`);
+                console.log(`Removing ${d.positions.size - nTreesInCohort} trees from cohort ${d.IID}, patch ${d.PID}`);
                 for (let i=nTreesInCohort; i<d.positions.size; i++) {
                     d.positions.delete(i);
                 }
                 // Mark cells as available
                 this.allCells.get(year).forEach(c=>{
-                    if (
-                        c.occupyingCohort === idFromData(d) &&
+                    if (c.occupyingCohort !== undefined &&
+                        idFromData(c.occupyingCohort) === idFromData(d) &&
                         c.occupyingInstance >= nTreesInCohort
                     ) {
                         c.occupyingCohort = undefined;
                         c.occupyingInstance = undefined;
                     }
                 });
-                this.availableCells.set(
-                    year, this.allCells.get(year).filter(c=>(
+                this.availableCells.set(year,
+                    this.allCells.get(year).filter(c => (
                         c.occupyingCohort === undefined &&
                         c.occupyingInstance === undefined
                     )
                 ));
+                console.log(`Available cells: ${this.availableCells.get(year).length}`)
             }
         }
 
         // Add new trees in each cohort
         for (const cohort of cohorts) {
-            const d = cohort.timeSteps.get(year)
+            const d = cohort.timeSteps.get(year);
             const nTreesInCohort = d.DensI * cohort.maxTreeCount;
 
             if (d.positions.size < nTreesInCohort) {
-                console.log(`Adding ${nTreesInCohort - d.positions.size} trees to cohort ${d.IID}, patch ${d.PID}. Available cells: ${this.availableCells.length}`);
+                console.log(`Adding ${nTreesInCohort - d.positions.size} trees to cohort ${d.IID}, patch ${d.PID}`);
             }
 
             while (d.positions.size < nTreesInCohort) {
@@ -192,21 +167,31 @@ class Patch {
                     crownRadius: crownRadius,
                     instanceId: d.positions.size
                 }
-                const cell = randItem(this.availableCells.get(year));
-                this.availableCells.set(
-                    year, this.availableCells.get(year).filter(
-                        c=>cell.distanceTo(c) > tree.crownRadius * this.allowedOverlap
+                // Choose an available cell at random
+                let cell = randItem(this.availableCells.get(year));
+
+                if (cell === undefined) {
+                    console.warn("No more space to place trees");
+                    cell = randItem(this.allCells.get(year));
+                }
+
+                // Mark cells as occupied
+                this.availableCells.set(year,
+                    this.availableCells.get(year).filter(
+                        c => cell.distanceTo(c) > tree.crownRadius * this.allowedOverlap
                     )
                 );
                 for (const c of this.allCells.get(year)) {
-                    if (cell.distanceTo(c) <= tree.crownRadius * this.allowedOverlap) {
-                        cell.occupyingCohort = idFromData(tree.data);
-                        cell.occupyingInstance = tree.instanceId;
+                    if (c.distanceTo(cell) <= tree.crownRadius * this.allowedOverlap) {
+                        c.occupyingCohort = cohort;
+                        c.occupyingInstance = tree.instanceId;
                     }
                 }
                 d.positions.set(tree.instanceId, cell);
             }
+            console.log(`Available cells: ${this.availableCells.get(year).length}`)
         }
+        this.positionedYears.add(year);
     }
 }
 
